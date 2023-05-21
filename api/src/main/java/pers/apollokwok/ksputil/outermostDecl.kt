2e34f3e6acf1df672d4d1c0c209c12313cdcbebc
WilliamKwokX <1425183716@qq.com>
2023-05-21 16:11:22 +0800

    +

--- api/src/main/java/pers/apollokwok/ksputil/outermostDecl.kt
+++ api/src/main/java/pers/apollokwok/ksputil/outermostDecl.kt
@@ -2,5 +2,7 @@ package pers.apollokwok.ksputil
 
 import com.google.devtools.ksp.symbol.KSDeclaration
 
+// todo: report this hint mistake
+@Suppress("RecursivePropertyAccessor")
 public val KSDeclaration.outermostDecl: KSDeclaration get() =
     parentDeclaration?.outermostDecl ?: this
